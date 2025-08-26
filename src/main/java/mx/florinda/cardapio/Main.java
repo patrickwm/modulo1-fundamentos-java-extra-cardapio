f1c0d12f4dfbcf0664a21ee495ef88afd9ab84e5
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2025-08-25 21:28:18 -0300

    Usando JDBC para Itens Cardapio

--- src/main/java/mx/florinda/cardapio/Main.java
+++ src/main/java/mx/florinda/cardapio/Main.java
@@ -2,27 +2,20 @@ package mx.florinda.cardapio;
 
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Database database = new Database();
+        Database database = new SQLDatabase();
 
-        ItemCardapio item = database.itemCardapioPorId(1L).orElseThrow(); // 2.99
+        List<ItemCardapio> listaItensCardapio = database.listaItensCardapio();
+        listaItensCardapio.forEach(System.out::println);
 
-        database.alteraPrecoItemCardapio(1L, new BigDecimal("3.99")); // 2.99 => 3.99
-        ItemCardapio item1 = database.itemCardapioPorId(1L).orElseThrow(); // 3.99
+        int total = database.totalItensCardapio();
+        System.out.println(total);
 
-        database.alteraPrecoItemCardapio(1L, new BigDecimal("2.99")); // 3.99 => 2.99
-        ItemCardapio item2 = database.itemCardapioPorId(1L).orElseThrow(); // 2.99
-
-        database.alteraPrecoItemCardapio(1L, new BigDecimal("4.99")); // 2.99 => 4.99
-        ItemCardapio item3 = database.itemCardapioPorId(1L).orElseThrow(); // 4.99
-
-        System.out.println("== " + (item == item2));
-        System.out.println("equals() " + (item.equals(item2)));
-        System.out.println("hashCode() " + (item.hashCode() == item2.hashCode()));
-
-        database.rastroAuditoriaPrecos();
+        var novoItemCardapio = new ItemCardapio(10L, "Tacos de Carnitas", "Incríveis tacos recheados com carne tenra", ItemCardapio.CategoriaCardapio.PRATOS_PRINCIPAIS, new BigDecimal("25.9"), null);
+        database.adicionaItemCardapio(novoItemCardapio);
     }
 
 }
--- src/main/java/mx/florinda/cardapio/ServidorItensCardapioComSocket.java
+++ src/main/java/mx/florinda/cardapio/ServidorItensCardapioComSocket.java
@@ -15,7 +15,7 @@ import java.util.concurrent.Executors;
 
 public class ServidorItensCardapioComSocket {
 
-    private static final Database database = new Database();
+    private static final Database database = new SQLDatabase();
 
     public static void main(String[] args) throws Exception {
 
