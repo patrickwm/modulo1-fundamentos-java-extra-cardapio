7b1c9bc27ffcec1d17665c89f1645039c2ffd4b6
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2025-08-31 20:12:55 -0300

    serialVersionUID

--- src/main/java/mx/florinda/cardapio/Pix.java
+++ src/main/java/mx/florinda/cardapio/Pix.java
@@ -2,17 +2,25 @@ package mx.florinda.cardapio;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.Objects;
 
 public class Pix implements Serializable {
+
+    private static final long serialVersionUID = 2L;
 
     private Long id;
     private BigDecimal valor;
     private String chaveDestino;
+    private Instant dataHora;
+    private String mensagem;
 
-    public Pix(Long id, BigDecimal valor, String chaveDestino) {
+    public Pix(Long id, BigDecimal valor, String chaveDestino, Instant dataHora, String mensagem) {
         this.id = id;
         this.valor = valor;
         this.chaveDestino = chaveDestino;
+        this.dataHora = dataHora;
+        this.mensagem = mensagem;
     }
 
     public Long getId() {
@@ -37,5 +45,44 @@ public class Pix implements Serializable {
 
     public void setChaveDestino(String chaveDestino) {
         this.chaveDestino = chaveDestino;
+    }
+
+    public Instant getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(Instant dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    @Override
+    public String toString() {
+        return "Pix{" +
+                "id=" + id +
+                ", valor=" + valor +
+                ", chaveDestino='" + chaveDestino + '\'' +
+                ", dataHora=" + dataHora +
+                ", mensagem='" + mensagem + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Pix pix = (Pix) o;
+        return Objects.equals(id, pix.id) && Objects.equals(valor, pix.valor) && Objects.equals(chaveDestino, pix.chaveDestino) && Objects.equals(dataHora, pix.dataHora) && Objects.equals(mensagem, pix.mensagem);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, valor, chaveDestino, dataHora, mensagem);
     }
 }
--- src/main/java/mx/florinda/cardapio/SerializadorPix.java
+++ src/main/java/mx/florinda/cardapio/SerializadorPix.java
@@ -3,12 +3,13 @@ package mx.florinda.cardapio;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 import java.math.BigDecimal;
+import java.time.Instant;
 
 public class SerializadorPix {
 
     public static void main(String[] args) throws Exception {
 
-        var pix = new Pix(1L, new BigDecimal("10.99"), "alexandre.aquiles@gmail.com");
+        var pix = new Pix(1L, new BigDecimal("10.99"), "alexandre.aquiles@gmail.com", Instant.now(), "😉");
         FileOutputStream fos = new FileOutputStream("pix.ser");
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         oos.writeObject(pix);
